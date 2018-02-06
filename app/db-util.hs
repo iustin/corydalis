015957e7b0800dc73bc8f46ca3f7f43e778baa40
Iustin Pop <iustin@k1024.org>
2018-02-06 23:14:26 +0100

    Initial version of a database maintenance tool
    Pfew, using persistent from scratch is no fun… At least
    optparse-applicative is straightforward. Will need to use that for
    main binary (loadYamlSettingsArgs is not nice).
    
    Note that for now this add users but without a password, thus they
    can't really be used yet.

---
+++ app/db-util.hs
@@ -0,0 +1,116 @@
+{-
+
+Copyright (C) 2013 Iustin Pop
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+-}
+
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE ScopedTypeVariables               #-}
+
+import Control.Monad.Logger
+import Database.Persist
+import Database.Persist.Sqlite
+import Import hiding (putStrLn)
+import Application
+import Model
+import Options.Applicative
+import Data.Semigroup ((<>))
+import Data.Text.IO
+
+data Options = Options
+  { optSettingsFile :: FilePath
+  , optCommand :: Command
+  }
+
+data Command = CmdAdd Text
+             | CmdDel Text
+             | CmdList
+
+parseAdd :: Parser Command
+parseAdd = CmdAdd <$> argument str (metavar "USERNAME")
+
+parseDel :: Parser Command
+parseDel = CmdDel <$> argument str (metavar "USERNAME")
+
+parseList :: Parser Command
+parseList = pure CmdList
+
+parseCommand :: Parser Command
+parseCommand = subparser $
+  command "add" (info parseAdd (progDesc "Add/update a user")) <>
+  command "del" (info parseDel (progDesc "Remove a user")) <>
+  command "list" (info parseList (progDesc "List the users"))
+
+parseOptions :: Parser Options
+parseOptions = Options
+  <$> strOption (long "config" <>
+                 short 'c' <>
+                 metavar "FILE" <>
+                 value "config/settings.yml" <>
+                 help "Load configuration settings from FILE"
+                )
+  <*> parseCommand
+
+upsertUser :: (MonadBaseControl IO m, MonadIO m)
+           => Text -> ReaderT SqlBackend (NoLoggingT (ResourceT m)) ()
+upsertUser user = do
+  insert (User user Nothing)
+  return ()
+
+removeUser :: (MonadBaseControl IO m, MonadIO m)
+           => Text -> ReaderT SqlBackend (NoLoggingT (ResourceT m)) ()
+removeUser user = do
+  userId <- getBy $ UniqueUser user
+  case userId of
+    Nothing -> liftIO $ hPutStrLn stderr $ concat ["User ", user, " not found. Ignoring."]
+    Just (Entity uId _) -> delete uId
+  return ()
+
+listUsers :: (MonadBaseControl IO m, MonadIO m)
+          => ReaderT SqlBackend (NoLoggingT (ResourceT m)) ()
+listUsers = do
+  users <- selectList [] [Asc UserName]
+  liftIO $ mapM_ (\(Entity _ (User name pw)) -> do
+                     putStrLn name
+                 ) users
+
+app :: (MonadBaseControl IO m, MonadIO m)
+    => Options -> ReaderT SqlBackend (NoLoggingT (ResourceT m)) ()
+app (Options _ cmd) =
+  case cmd of
+    CmdAdd u -> upsertUser u
+    CmdDel u -> removeUser u
+    CmdList -> listUsers
+
+main :: IO ()
+main = do
+    opts <- execParser (info (helper <*> parseOptions) $ progDesc "Corydalis database maintenance")
+
+    -- Get the settings from all relevant sources
+    appSettings <- loadYamlSettings
+        [optSettingsFile opts]
+        -- fall back to compile-time values, set to [] to require values at runtime
+        [configSettingsYmlValue]
+        -- allow environment variables to override
+        useEnv
+    let conn = sqlDatabase $ appDatabaseConf appSettings
+
+    runStderrLoggingT $ runSqlite conn $ app opts
+
