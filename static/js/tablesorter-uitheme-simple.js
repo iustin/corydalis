2ae4ca90ff558bea0a41485c847a4cbf5a28e72e
Iustin Pop <iustin@k1024.org>
2018-02-20 03:56:18 +0100

    Switch back to standard tablesorter bootstrap theme
    I did not realize that simply defining a new theme means all the CSS
    is lost - which resulted in no so nice e.g. filter-disabled
    icons (very big and ugly).
    
    Now the overrides are much more clear as well.
    
    Also switch back to enabling the filter bar always, as 1) hovering on
    mobile would be tricky, and 2) the hiding (via opacity) doesn't play
    nice with local CSS.

--- static/js/tablesorter-uitheme-simple.js
+++ static/js/tablesorter-uitheme-simple.js
@@ -3,29 +3,13 @@
     'use strict';
     var ts = $.tablesorter || {};
 
-    ts.themes = {
-        'bootstrap-simple' : {
-            table        : 'table table-bordered',
-            caption      : 'caption',
-            // header class names
-            header       : 'bootstrap-header', // give the header a gradient background (theme.bootstrap_2.css)
-            sortNone     : '',
-            sortAsc      : '',
-            sortDesc     : '',
-            active       : '', // applied when column is sorted
-            hover        : '', // custom css required - a defined bootstrap style may not override other classes
-            // icon class names
-            icons        : '', // add 'icon-white' to make them white; this icon class is added to the <i> in the header
-            iconSortNone : 'fa fa-sort', // class name added to icon when column is not sorted
-            iconSortAsc  : 'fa fa-sort-asc', // class name added to icon when column has ascending sort
-            iconSortDesc : 'fa fa-sort-desc', // class name added to icon when column has descending sort
-            filterRow    : '', // filter row class
-            footerRow    : '',
-            footerCells  : '',
-            even         : '', // even row zebra striping
-            odd          : ''  // odd row zebra striping
-        }
-    };
+    $.extend(ts.themes.bootstrap, {
+        table        : 'table table-bordered',
+        icons        : '', // add 'icon-white' to make them white; this icon class is added to the <i> in the header
+        iconSortNone : 'fa fa-sort', // class name added to icon when column is not sorted
+        iconSortAsc  : 'fa fa-sort-asc', // class name added to icon when column has ascending sort
+        iconSortDesc : 'fa fa-sort-desc', // class name added to icon when column has descending sort
+    });
 
 
 })(jQuery);
