afecc46204df7efd38d8bf6e27e5c53d703d2d11
Iustin Pop <iustin@k1024.org>
2018-02-21 00:19:58 +0100

    Move icon names over to FA 5 names
    Hmm… some look uglier. Strange.

--- static/js/tablesorter-uitheme-simple.js
+++ static/js/tablesorter-uitheme-simple.js
@@ -6,9 +6,9 @@
     $.extend(ts.themes.bootstrap, {
         table        : 'table',
         icons        : '', // add 'icon-white' to make them white; this icon class is added to the <i> in the header
-        iconSortNone : 'fa fa-sort', // class name added to icon when column is not sorted
-        iconSortAsc  : 'fa fa-sort-asc', // class name added to icon when column has ascending sort
-        iconSortDesc : 'fa fa-sort-desc', // class name added to icon when column has descending sort
+        iconSortNone : 'fas fa-sort', // class name added to icon when column is not sorted
+        iconSortAsc  : 'fas fa-sort-up', // class name added to icon when column has ascending sort
+        iconSortDesc : 'fas fa-sort-down', // class name added to icon when column has descending sort
     });
 
 
