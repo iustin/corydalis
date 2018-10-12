d5e457576ab8dbe28404ad9f851cec34090b34f4
Iustin Pop <iustin@k1024.org>
2018-10-12 14:33:47 +0200

    Remove now-unused pragma

--- src/Pics.hs
+++ src/Pics.hs
@@ -20,7 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 {-# LANGUAGE BangPatterns      #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
-{-# LANGUAGE ViewPatterns      #-}
 
 module Pics ( PicDir(..)
             , Image(..)
