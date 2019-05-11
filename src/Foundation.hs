ffee83ae50df5729c989a58c71603ae4992d053d
Iustin Pop <iustin@k1024.org>
2019-05-11 23:47:07 +0200

    Rename default-message-widget to message-widget
    Again, better tab completion.

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -265,8 +265,8 @@ instance Yesod App where
     makeLogger = return . appLogger
 
     -- Provide proper Bootstrap styling for default displays, like
-    -- error pages
-    defaultMessageWidget title body = $(widgetFile "default-message-widget")
+    -- error pages.
+    defaultMessageWidget title body = $(widgetFile "message-widget")
 
 -- Define breadcrumbs.
 instance YesodBreadcrumbs App where
