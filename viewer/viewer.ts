d2104606a1a93f0d288940d0a401945f81d289d5
Iustin Pop <iustin@k1024.org>
2020-06-06 23:29:44 +0200

    viewer: Add explicit type references
    It turns out I kind of misunderstood why screenfull symbols got
    unbroken - I had by accident a @types/screenfull with the .d.ts file
    copied from the main (non-types) package. Clearing out node_modules
    and re-running `npm install` (due to another mishap) resulted in
    screenfull being broken again :(
    
    After some research, it turns out that either `reference types` or
    `types` compiler option works. I chose the former since it's more
    correct, it declares correctly the dependencies for this file (and
    only this file).
    
    Too bad jquery/hammerjs were inferred automatically. Or rather, too
    good that I used one module that wasn't :)

--- viewer/viewer.ts
+++ viewer/viewer.ts
@@ -15,6 +15,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
+/// <reference types="jquery"/>
+/// <reference types="hammerjs"/>
+/// <reference types="screenfull"/>
 
 // The Haskell types.
 
