df0e7f38cd19ce68117025da11aed5ec1b7787e5
Iustin Pop <iustin@k1024.org>
2020-06-08 04:34:10 +0200

    Run eslint --fix on lensinfo.ts
    So easy :)

--- js/lensinfo.ts
+++ js/lensinfo.ts
@@ -1,38 +1,36 @@
 /// <reference types="plotly.js"/>
 
 $(document).ready(function() {
+  function onDataReceived(series: any) {
+    const lensLayout = {
+      xaxis: {
+        title: 'Focal length',
+      },
+      yaxis: {
+        title: 'Aperture',
+        tickprefix: 'f/',
+        tickvals: series.ytickvals,
+        ticktext: series.yticktext,
+        type: 'log' as const,
+      },
+      hovermode: 'closest' as const,
+    };
+    const config = {
+      showLink: false,
+      sendData: false,
+      displaylogo: false,
+      modeBarButtonsToRemove: ['toImage' as const, 'sendDataToCloud' as const],
+    };
+    Plotly.newPlot('lensFlApChart', series.lensflap, lensLayout, config);
+  }
 
-    function onDataReceived(series: any) {
+  const bootdiv = $('#boot');
+  const lensinfourl = bootdiv.data('lensinfo-url');
 
-        var lensLayout = {
-            xaxis: {
-                title: 'Focal length'
-            },
-            yaxis: {
-                title: 'Aperture',
-                tickprefix: 'f/',
-                tickvals: series.ytickvals,
-                ticktext: series.yticktext,
-                type: 'log' as const,
-            },
-            hovermode: 'closest' as const,
-        }
-        var config = {
-            showLink: false,
-            sendData: false,
-            displaylogo: false,
-            modeBarButtonsToRemove: [ 'toImage' as const, 'sendDataToCloud' as const ]
-        }
-        Plotly.newPlot('lensFlApChart', series.lensflap, lensLayout, config);
-    }
-
-    const bootdiv = $('#boot');
-    const lensinfourl = bootdiv.data('lensinfo-url');
-
-    $.ajax({
-        url: lensinfourl,
-        type: "GET",
-        dataType: "json",
-        success: onDataReceived
-    });
+  $.ajax({
+    url: lensinfourl,
+    type: 'GET',
+    dataType: 'json',
+    success: onDataReceived,
+  });
 });
