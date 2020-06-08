50ba11222c9d5716c67cd0c5f6d9101c7210769b
Iustin Pop <iustin@k1024.org>
2020-06-08 03:06:53 +0200

    Fixup fancybox animation/transitions setting
    Thanks to TS for this. Sadly, can't really enable, types definition
    is not very good.
    
    But wow, this makes fancybox feel "fast"…

--- js/fancybox.js
+++ js/fancybox.js
@@ -17,8 +17,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 $(document).ready(function() {
   'use strict';
-  $.fancybox.defaults.animationEffect = 'false';
-  $.fancybox.defaults.transitionEffect = 'false';
+  $.fancybox.defaults.animationEffect = false;
+  $.fancybox.defaults.transitionEffect = false;
   // Disable hashing since masonry controls the URL.
   $.fancybox.defaults.hash = false;
   // We know we act on a grid, which has infinite scroll enabled.
