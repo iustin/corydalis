eb10465f20ac60e313dcba5beeee3417ffb15bf2
Iustin Pop <iustin@k1024.org>
2020-06-11 21:29:19 +0200

    Update dirlocals
    70 chars for comment wrapping is a bit small. I'd prefer more, but
    let's use 80 for now.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,11 +1,15 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((hamlet-mode
+((nil
+  (fill-column . 80))
+ (hamlet-mode
   (hamlet/basic-offset . 2)
   (haskell-process-use-ghci . t))
  (haskell-mode
   (haskell-tags-on-save . t)
   (haskell-stylish-on-save . t)
   (haskell-indent-spaces . 2)
-  (haskell-process-use-ghci . t)))
+  (haskell-process-use-ghci . t))
+ (".git"
+  (nil (fill-column . 70))))
