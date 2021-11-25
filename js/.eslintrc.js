fe492a49a7f868a5ec4057e4ab756d6be4a622d8
Iustin Pop <iustin@k1024.org>
2021-11-26 00:11:12 +0100

    Fix eslint config file
    Simple `npx eslint --fix js/` run.

--- js/.eslintrc.js
+++ js/.eslintrc.js
@@ -15,20 +15,20 @@ module.exports = {
   ],
   'rules': {
     'indent': ['error', 2,
-               { 'VariableDeclarator': 'first' ,
-                 'FunctionDeclaration': {'parameters': 'first'} ,
-                 'FunctionExpression': {'parameters': 'first'} ,
-                 'CallExpression': {'arguments': 'first'} ,
-                 'ArrayExpression': 'first' ,
-                 'ObjectExpression': 'first' ,
-                 'ImportDeclaration': 'first',
-                 'SwitchCase': 1,
-               }
-              ],
-    'spaced-comment': ['error', 'always', { 'markers': ['/'] }],
+               {'VariableDeclarator': 'first',
+                'FunctionDeclaration': {'parameters': 'first'},
+                'FunctionExpression': {'parameters': 'first'},
+                'CallExpression': {'arguments': 'first'},
+                'ArrayExpression': 'first',
+                'ObjectExpression': 'first',
+                'ImportDeclaration': 'first',
+                'SwitchCase': 1,
+               },
+    ],
+    'spaced-comment': ['error', 'always', {'markers': ['/']}],
     'require-jsdoc': ['off'],
     'new-cap': ['error', {
-      'capIsNewExceptions': ['LOG', 'T_START', 'T_STOP']
-    }]
+      'capIsNewExceptions': ['LOG', 'T_START', 'T_STOP'],
+    }],
   },
 };
